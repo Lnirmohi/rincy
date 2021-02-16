34b9fd168765b130477ec125b534acac87f2ef83
Laxmikant Nirmohi <laxmikant.nirmohi@gmail.com>
2021-02-16 16:56:12 +0530

    add MediaQueryList and add eventListener - navbarListener to toggle mobileNavbarDisplay's value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'vedamrit';
+  displayMobileNavbar: boolean;
+  mql: MediaQueryList;
+
+  constructor() {
+    this.displayMobileNavbar = false;
+    this.mql = window.matchMedia('screen and (max-width: 986px)');
+  }
+
+  ngOnInit(): void {
+    this.mql.addEventListener('change', this.navbarListener);
+  }
+
+  // to preserve this(component context) use arrow function
+  // otherwise this context gets assigned to mediaMatch
+  // because of eventListener
+  navbarListener = (mediaMatch) => {
+
+    if (mediaMatch.matches) {
+      console.log('this.displayMobileNavbar ', this.displayMobileNavbar);
+      this.displayMobileNavbar = true;
+    } else {
+      console.log('this.displayMobileNavbar ', this.displayMobileNavbar);
+      this.displayMobileNavbar = false;
+    }
+
+  }
+
 }
