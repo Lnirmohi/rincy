e6863308087965f96bc76647cdb04aeddc04869a
Laxmikant Nirmohi <laxmikant.nirmohi@gmail.com>
2021-03-24 16:14:12 +0530

    remove main-header, primary-header and mobile-header components from declaration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,31 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   title = 'vedamrit';
-  displayMobileNavbar: boolean;
-  mql: MediaQueryList;
+
 
   constructor() {
-    this.displayMobileNavbar = false;
-    this.mql = window.matchMedia('screen and (max-width: 986px)');
+
   }
 
   ngOnInit(): void {
-    this.navbarListener(this.mql);
-    this.mql.addEventListener('change', this.navbarListener);
-  }
-
-  // to preserve this(component context) use arrow function
-  // otherwise this context gets assigned to mediaMatch
-  // because of eventListener
-  navbarListener = (mediaMatch) => {
-
-    if (mediaMatch.matches) {
-      console.log('this.displayMobileNavbar ', this.displayMobileNavbar);
-      this.displayMobileNavbar = true;
-    } else {
-      console.log('this.displayMobileNavbar ', this.displayMobileNavbar);
-      this.displayMobileNavbar = false;
-    }
 
   }
 
