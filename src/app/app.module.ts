9ff59b76305e376e6993c6969d1d35caa9332ae8
Laxmikant Nirmohi <laxmikant.nirmohi@gmail.com>
2021-03-24 16:18:00 +0530

    remove main-header, primary-header and mobile-header components from declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,15 @@ import { NgxSiemaModule} from 'ngx-siema';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrimaryHeaderComponent } from './primary-header/primary-header.component';
-import { MainHeaderComponent } from './main-header/main-header.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { FooterComponent } from './footer/footer.component';
-import { MobileNavbarComponent } from './mobile-navbar/mobile-navbar.component';
-import { FeatureCarouselComponent } from './feature-carousel/feature-carousel.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimaryHeaderComponent,
-    MainHeaderComponent,
-    NavMenuComponent,
     FooterComponent,
-    MobileNavbarComponent,
-    FeatureCarouselComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
